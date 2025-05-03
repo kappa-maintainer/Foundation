5ce99a6e8f6056c3b992248cff9391adaba96ff1
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-05-04 00:18:39 +0800

    Improve the class cache

--- src/main/java/top/outlands/foundation/boot/ActualClassLoader.java
+++ src/main/java/top/outlands/foundation/boot/ActualClassLoader.java
@@ -180,6 +180,11 @@ public class ActualClassLoader extends URLClassLoader {
 
         if (cachedClasses.containsKey(name)) {
             return cachedClasses.get(name);
+        } else { // What if a class doesn't get cached?
+            Class<?> fc = findLoadedClass(name);
+            if (fc != null) {
+                cachedClasses.put(name, fc);
+            }
         }
 
         byte[] transformedClass;
@@ -551,7 +556,7 @@ public class ActualClassLoader extends URLClassLoader {
      */
     public boolean isClassExist(String name) {
         try {
-            return testGetClassBytes(name) != null;
+            return testGetClassBytes(name) != null || isClassLoaded(name);
         } catch (Throwable e) {
             return false;
         }
