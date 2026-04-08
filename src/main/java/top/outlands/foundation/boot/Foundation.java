ab926952799fc6811406dfe82d149965f0b0c497
arcade_kappa <gkappa@outlands.top>
2026-04-08 11:15:44 +0800

    Fix main function

--- src/main/java/top/outlands/foundation/boot/Foundation.java
+++ src/main/java/top/outlands/foundation/boot/Foundation.java
@@ -27,7 +27,7 @@ public class Foundation {
         }
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> Foundation.LOGGER.error(thread, throwable));
         try {
             breakModuleAndReflection();
